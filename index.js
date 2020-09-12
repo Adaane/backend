fe01b1d2868d07e06f29d3151651ab0b3f104581
Adaane <adaane.ahamada@hetic.net>
2020-09-12 21:23:13 +0200

    configurer la base de données

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 // const express = require('express')
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes'
 
 const app = express();
 const PORT = 3000;
+
+// connexion mongoose
+mongoose.Promise = new Promise();
+mongoose.connect('mongodb://localhost/CRMdb', {
+    useMongoClient: true
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 routes(app);
 
