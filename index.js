b376ca7437a6a398fef3faab18e2e99fb03bde76
Adaane <adaane.ahamada@hetic.net>
2020-09-12 18:47:37 +0200

    configuration initial du serveur

---
+++ index.js
@@ -0,0 +1,13 @@
+// const express = require('express')
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => (
+    res.send(`Serveur node et express sur port ${PORT}`)
+));
+
+app.listen(PORT, () => (
+    console.log(`votre serveur est sur le port ${PORT}`)
+));
