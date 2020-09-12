43b3ccf97728b30819b713eb839f6492e55db8c0
Adaane <adaane.ahamada@hetic.net>
2020-09-12 21:04:23 +0200

    les bases des middlewares

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,8 +1,16 @@
 const routes = (app) => {
     app.route('/contact')
-    .get((req, res) => (
+    .get((req, res, next ) => {
+        //middleware
+        /**Un middleware est simplement une fonction qui se situe entre la requette (req) et la reponse (res) */
+
+        console.log(`Request de ${req.orginalUrl}` )
+        console.log(`Request type ${req.method}` )
+        next();
+    }, (req, res, next ) => (
         res.send('demande GET avec succès')
     ))
+    
     .post((req, res) => (
         res.send('demande POST avec succès')
     ))
